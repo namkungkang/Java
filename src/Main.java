59d0eba9b501b7eda92212d837250869660f3ad9
namkungkang <namkung0131@naver.com>
2025-02-06 16:12:31 +0900

    [FAET] #3 문자열, 특수문자

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        //문자열
+        String s = "i like java";
+        System.out.println(s.length()); //길이
+        System.out.println(s.toUpperCase()); //대문자
+        System.out.println(s.toLowerCase()); //소문자
+        System.out.println(s.contains("java")); //포함하나요?
+        System.out.println(s.indexOf("like")); // like 어디 인덱스에 있나요?
+        System.out.println(s.lastIndexOf("v")); // 마지막 인덱스가 v인가요?
+        System.out.println(s.startsWith("i")); // i로 시작하나요?
+        System.out.println(s.endsWith("i")); // i로 끝나나요?
+        System.out.println(s.replace("like","love")); // like를 love로 바꿔주세요.
+        System.out.println(s.substring(8)); // 8까지 자르기
+        System.out.println(s.trim()); // 앞뒤 공백 제거
+        System.out.println(s.concat("and Python")); // 뒤에 결합
 
-        // 형 변환 int -> float
-        int score = 93;
-        float score_f = (float)score;
-        System.out.println(score_f);
-
-        // 산술 연산자
-        int a = 1; int b = 2;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(++a); //전위 연산
-        System.out.println(a++); //후위 연산
-        System.out.println(a--);
-        System.out.println(a);
-
-        // 대입 연산자
-        int num =10;
-        num = num + 2;
-        System.out.println(num);
-
-        // 복합 연산자
-        int number = 10;
-        number += 10;
-        System.out.println(number);
-
-        // 비교 연산자
-        boolean bool = 1 != 2;
-        System.out.println(bool);
-
-        // 논리 연산자
-        boolean bb = (5>3) || (4>2);
-        System.out.println(bb);
-
-        //삼항 연산자
-        int x = 3; int y =5;
-        int max = x>y ? x:y;
-        System.out.println(max);
-
-
-
-
+        //특수 문자
+        /* \n : 줄바꿈
+           \t : 탭
+           \\ : 역슬래시
+           \" : 큰따옴표
+           \' : 작은따옴표
+         */
     }
 }
 // 기본 자료형 : 실제 데이터 값 저장 (ex.int,float,double,boolean)
@@ -54,7 +35,7 @@ public class Main {
 2. 밑줄 또는 문자로 시작 가능
 3. 한 단어 또는 2개 이상 단어의 연속
 4. 소문자로 시작, 각 단어의 시작 글자는 대문자(첫 단어 제외)
-5.예약어 X(public, void, static, int, double, float)
+5. 예약어 X(public, void, static, int, double, float)
 6. 대소문자 구분(myName과 myname은 다른 변수)
 */
 
