47db4545ef3619fc1927d5b156ca55bb625f75b5
namkungkang <namkung0131@naver.com>
2025-02-06 15:03:13 +0900

    [FEAT] #1 자바 세팅

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello ");
-
-
+        System.out.printf("Hello    \n ");
+        System.out.printf("하이");
     }
 }
