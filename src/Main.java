070a0300b54286bd50976eba344722bbcaec29b6
namkungkang <namkung0131@naver.com>
2025-02-06 15:15:51 +0900

    [FEAT] #2 datatype

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello    \n ");
-        System.out.printf("하이");
-    }
+        int a = 1;
+        System.out.println(a);
+
+
+        double b = 90.5;
+        System.out.println(b);
+
+        }
 }
+// 기본 자료형 : 실제 데이터 값 저장 (ex.int,float,double,boolean)
+// 참조 자료형 : 데이터가 저장된 메모리 주소 값 저장 (ex. string)
+// 변수 : 자료 값을 저장 하는 공간 (ex. int hour = 15;)
+
